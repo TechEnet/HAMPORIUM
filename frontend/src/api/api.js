cf45e2aacb0825cee215b9a721759d6abe235bd5
Suryansh Patel <suryansh30patel@gmail.com>
2026-09-22 14:25:51 +0530

    changes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "http://localhost:5000/api",
+    // "http://localhost:5000/api",
+    "https://hamporium.onrender.com/api",
   withCredentials: true,
 });
 
